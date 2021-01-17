c26526e10ebf3ef0818e0b99fa740daa2795379e
harvey magana <heyharv2012@gmail.com>
2021-01-17 09:23:25 -0800

    Registered the get method in the server file that is coming from the posts-routes file.

--- api/posts/posts-routes.js
+++ api/posts/posts-routes.js
@@ -2,12 +2,17 @@
 // a router variable is created from the express.Router() method
 // the router includes all of the http methods with the logic that uses the model
 // methods 
+// next, you need to import the routes file into the server file by declaring it as a 
+// unique variable with includes its path
+// in the server file, you also need to register the router
 
 const express = require('express');
 const router = express.Router();
 
+const Posts = require('./posts-model.js');
+
 router.get('/api/posts', (req, res) => {
-    Posts .find(req.query)
+    Posts.find(req.query)
         .then(posts => {
             res.status(200).json(posts);
         })
--- api/server.js
+++ api/server.js
@@ -2,7 +2,11 @@ const express = require('express');
 
 const server = express();
 
-server.use(express.json())
+const PostsRouter = require('./posts/posts-routes.js');
+
+server.use(express.json());
+
+server.use('/api/posts', PostsRouter);
 
 server.get('/', (req, res) => {
     res.json({ message: "Hello world!" });
