cfaa17cac4935dd2bd6beacbb939e3cccf346ed7
harvey magana <heyharv2012@gmail.com>
2021-01-17 09:07:35 -0800

    Created a posts and comments directory and added the model file and routes file for the posts and comments in the api directory.

---
+++ api/posts/posts-model.js
@@ -0,0 +1,3 @@
+// the model file starts by importing the databased config file, in our case, it is lambda.db3
+// the model includes all of the database methods 
+// the model then exports all of the database methods 
---
+++ api/posts/posts-routes.js
@@ -0,0 +1,6 @@
+// the router imports the associated model file 
+// a router variable is created from the express.Router() method
+// the router includes all of the http methods with the logic that uses the model
+// methods 
+
+const express = require('express');
