bef301831d353c6475662d7355672057383baeb6
harvey magana <heyharv2012@gmail.com>
2021-01-17 09:12:51 -0800

    Started the code with one get method in the posts-routes file.

--- api/posts/posts-routes.js
+++ api/posts/posts-routes.js
@@ -4,3 +4,19 @@
 // methods 
 
 const express = require('express');
+const router = express.Router();
+
+router.get('/api/posts', (req, res) => {
+    Posts .find(req.query)
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                message: "Error retrieving the posts"
+            })
+        })
+})
+
+module.exports = router;
