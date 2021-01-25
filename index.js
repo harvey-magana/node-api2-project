ad71ea7a9f3c53bbcdac2a5a6922b29e1007c854
harvey magana <heyharv2012@gmail.com>
2021-01-24 16:56:49 -0800

    imported dotenv, added the .env file to the root directory, added the port variable to index.js and added the port and value to the .env file.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require('./api/server.js');
 
-const PORT = 5000;
+const port = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
-    console.log(`\n*** server listening on port ${PORT} ***\n`)
+server.listen(port, () => {
+    console.log(`\n*** server listening on port ${port} ***\n`)
 })
